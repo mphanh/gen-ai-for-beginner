687c3221ca818486e10049eae9f9b813a3c838a1
ANH MAI PHUONG <ntt.mai_phuong@extaccount.com>
2024-10-11 14:29:34 +0700

    llama

---
+++ function-calling-llama/app.py
@@ -0,0 +1,39 @@
+import os
+from azure.ai.inference import ChatCompletionsClient
+from azure.ai.inference.models import AssistantMessage, SystemMessage, UserMessage
+from azure.core.credentials import AzureKeyCredential
+import dotenv
+
+# Load environment variables from .env file
+dotenv.load_dotenv()
+
+token = os.environ["GITHUB_TOKEN"]
+endpoint = "https://models.inference.ai.azure.com"
+model_name = "meta-llama-3.1-405b-instruct"
+
+client = ChatCompletionsClient(
+    endpoint=endpoint,
+    credential=AzureKeyCredential(token),
+)
+
+
+tool_prompt=f"""
+<|begin_of_text|><|start_header_id|>system<|end_header_id|>
+
+Environment: ipython
+Tools: brave_search, wolfram_alpha
+Cutting Knowledge Date: December 2023
+Today Date: 23 July 2024
+
+You are a helpful assistant<|eot_id|>
+"""
+
+messages = [
+    SystemMessage(content=tool_prompt),
+    UserMessage(content="What is the weather in Stockholm?"),
+
+]
+
+response = client.complete(messages=messages, model=model_name)
+
+print(response.choices[0].message.content)
